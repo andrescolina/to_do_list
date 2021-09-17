a09af47f092c20db88ef2ff2e79394b1d2df2275
Andres <andres.colina@grupor5.com>
2021-09-17 14:49:15 -0500

    first commit

---
+++ src/to_do_list/menu.py
@@ -0,0 +1,35 @@
+from .task import Task
+from time import sleep
+
+
+class Menu:
+
+    def __init__(self):
+        self.menu = {}
+        self.instance = Task()
+
+    def execute(self):
+        select = self.print_menu()
+        self.select_action(select)
+
+    def print_menu(self):
+        self.menu['1'] = {'name': "Create Task.", 'function': self.instance.create, 'sleep_time': 2}
+        self.menu['2'] = {'name': "Updated Task.", 'function': self.instance.get, 'sleep_time': 3}
+        self.menu['3'] = {'name': "Delete Task.", 'function': self.instance.get, 'sleep_time': 3}
+        self.menu['4'] = {'name': "List Task.", 'function': self.instance.get, 'sleep_time': 3}
+        self.menu['exit'] = {'name': "Exit"}
+        options = self.menu.keys()
+        for entry in options:
+            print(entry, self.menu[entry]['name'])
+        select_user = input("What would you like to do? ")
+        return select_user
+
+    def select_action(self, element_select):
+        if element_select == 'exit':
+            return
+        elif element_select not in self.menu.keys():
+            self.execute()
+        else:
+            self.menu[element_select]['function']()
+            sleep(self.menu[element_select]['sleep_time'])
+            self.execute()
---
+++ src/to_do_list/task.py
@@ -0,0 +1,64 @@
+import datetime
+from tabulate import tabulate
+
+
+class Task:
+
+    def __init__(self):
+        self.tasks = {}
+        self.fields = {
+            'title': {
+                'name': 'Title of task: ',
+                'options': [],
+                'required': True
+            },
+            'content': {
+                'name': 'Content of task: ',
+                'options': [],
+                'required': True
+            },
+            'assign_to': {
+                'name': 'Assign task to: ',
+                'options': [],
+                'required': True
+            },
+            'status': {
+                'name': 'Status of task: ',
+                'options': ['approved', 'reproved', 'in_progress', 'complete'],
+                'required': True
+            }
+        }
+
+    def valide_data(self, field):
+        data_field = input(self.fields[field]['name'])
+        if self.fields[field]['required'] and len(data_field) == 0:
+            return self.valide_data(field)
+        elif len(self.fields[field]['options']) > 0:
+            if not data_field in self.fields[field]['options']:
+                print(f'Digit to valid options {",".join(self.fields[field]["options"])}')
+                return self.valide_data(field)
+            else:
+                return data_field
+        else:
+            return data_field
+
+    def create(self):
+        id_task = max(self.tasks.keys()) + 1 if len(self.tasks) > 0 else 1
+        self.tasks[id_task] = {}
+        for field in self.fields:
+            data_field = self.valide_data(field)
+            self.tasks[id_task][field] = data_field
+        self.tasks[id_task]['create_date'] = datetime.datetime.now().strftime('%YY/%m/%d %H:%M:%S')
+        print('Create success')
+        return True
+
+    def get(self):
+        headers = ['Title of task', 'Content of task', 'Assign task to', 'Status of task', 'Date created']
+        results = []
+        for task in self.tasks:
+            result = [self.tasks[task]['title'], self.tasks[task]['content'], self.tasks[task]['assign_to'],
+                      self.tasks[task]['status'], self.tasks[task]['create_date']
+                      ]
+            results.append(result)
+        print(tabulate(results, headers=headers))
+        return True
